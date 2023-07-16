792c1dea1a11b107ce58b1566d051800ce5f865a
Louise Findlay <louise.findlay@prismic.io>
2023-07-16 18:42:27 +0100

    Added other pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
 export default defineConfig({
-  integrations: [image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  })]
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
